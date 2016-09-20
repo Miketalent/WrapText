660ae8b4198e157fee6c94e734f0012011300261
Miketalent <Mithely@Michaels-iMac.local>
2016-09-19 19:32:22 -0700

    ColumnEqualToInputStringLengthTestFailed
    added unit test

--- WrapTextTest/WrapTextTest.cs
+++ WrapTextTest/WrapTextTest.cs
@@ -17,5 +17,16 @@ namespace WrapTextTest
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void ShallReturnOriginalStrIfColumnEqualToInputStrLength()
+        {
+            var input = "abc";
+            var expected = "abc";
+            var actual = WrapTextUtil.wrapText(input, input.Length);
+
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
