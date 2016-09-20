3dbb1fb71821092dc682ed98c2a6ccb7496972b8
Miketalent <Mithely@Michaels-iMac.local>
2016-09-19 19:43:10 -0700

    ColumnLessThanInputStrLengthTestsFailed
    1. added tests

--- WrapTextTest/WrapTextTest.cs
+++ WrapTextTest/WrapTextTest.cs
@@ -28,5 +28,29 @@ namespace WrapTextTest
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void ShallInsertLineBreakAtIndex2IfInputStrLengthIs3AndColumnIs2()
+        {
+            var input = "abc";
+            var expected = "ab\nc";
+            var actual = WrapTextUtil.wrapText(input, 2);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void ShallInsertLineBreakAtIndex2IfInputStrLengthIs4AndColumnIs2()
+        {
+            var input = "abcd";
+            var expected = "ab\ncd";
+            var actual = WrapTextUtil.wrapText(input, 2);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
     }
 }
